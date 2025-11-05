5f2b128b95ebfbd87360626b7b0a6d621b2cdbb7
Matias <matiaslucero019@gmail.com>
2025-11-05 20:23:14 -0300

    Cardrep - Blackjack y Truco con envido

---
+++ CocoR v4/Parser.cpp
@@ -0,0 +1,414 @@
+
+
+#include <wchar.h>
+#include "Parser.h"
+#include "Scanner.h"
+
+
+
+
+void Parser::SynErr(int n) {
+	if (errDist >= minErrDist) errors->SynErr(la->line, la->col, n);
+	errDist = 0;
+}
+
+void Parser::SemErr(const wchar_t* msg) {
+	if (errDist >= minErrDist) errors->Error(t->line, t->col, msg);
+	errDist = 0;
+}
+
+void Parser::Get() {
+	for (;;) {
+		t = la;
+		la = scanner->Scan();
+		if (la->kind <= maxT) { ++errDist; break; }
+
+		if (dummyToken != t) {
+			dummyToken->kind = t->kind;
+			dummyToken->pos = t->pos;
+			dummyToken->col = t->col;
+			dummyToken->line = t->line;
+			dummyToken->next = NULL;
+			coco_string_delete(dummyToken->val);
+			dummyToken->val = coco_string_create(t->val);
+			t = dummyToken;
+		}
+		la = t;
+	}
+}
+
+void Parser::Expect(int n) {
+	if (la->kind==n) Get(); else { SynErr(n); }
+}
+
+void Parser::ExpectWeak(int n, int follow) {
+	if (la->kind == n) Get();
+	else {
+		SynErr(n);
+		while (!StartOf(follow)) Get();
+	}
+}
+
+bool Parser::WeakSeparator(int n, int syFol, int repFol) {
+	if (la->kind == n) {Get(); return true;}
+	else if (StartOf(repFol)) {return false;}
+	else {
+		SynErr(n);
+		while (!(StartOf(syFol) || StartOf(repFol) || StartOf(0))) {
+			Get();
+		}
+		return StartOf(syFol);
+	}
+}
+
+void Parser::Cardrep() {
+		Expect(3 /* "JUEGO" */);
+		Expect(_ident);
+		gameType = wcharToString(t->val); 
+		Estado();
+		Expect(4 /* "PREDECIR" */);
+		Expect(5 /* "jugada" */);
+}
+
+void Parser::Estado() {
+		if (la->kind == 6 /* "BLACKJACK_JUGADOR" */) {
+			EstadoBlackjack();
+		} else if (la->kind == 12 /* "TRUCO_JUGADOR" */) {
+			EstadoTruco();
+		} else SynErr(17);
+}
+
+void Parser::EstadoBlackjack() {
+		Expect(6 /* "BLACKJACK_JUGADOR" */);
+		Expect(7 /* "[" */);
+		ListaCartasJugador();
+		Expect(8 /* "]" */);
+		Expect(9 /* "BLACKJACK_DEALER" */);
+		Expect(7 /* "[" */);
+		ListaCartasDealer();
+		Expect(8 /* "]" */);
+}
+
+void Parser::EstadoTruco() {
+		Expect(12 /* "TRUCO_JUGADOR" */);
+		Expect(7 /* "[" */);
+		ListaCartasTrucoJugador();
+		Expect(8 /* "]" */);
+		Expect(13 /* "TRUCO_OPONENTE" */);
+		Expect(7 /* "[" */);
+		ListaCartasTrucoOponente();
+		Expect(8 /* "]" */);
+		Expect(14 /* "TRUCO_MESA_JUGADOR" */);
+		Expect(7 /* "[" */);
+		ListaCartasMesaJugador();
+		Expect(8 /* "]" */);
+		Expect(15 /* "TRUCO_MESA_OPONENTE" */);
+		Expect(7 /* "[" */);
+		ListaCartasMesaOponente();
+		Expect(8 /* "]" */);
+}
+
+void Parser::ListaCartasJugador() {
+		CartaJugador();
+		while (la->kind == 10 /* "," */) {
+			Get();
+			CartaJugador();
+		}
+}
+
+void Parser::ListaCartasDealer() {
+		CartaDealer();
+		while (la->kind == 10 /* "," */) {
+			Get();
+			CartaDealer();
+		}
+}
+
+void Parser::CartaJugador() {
+		ValorCarta();
+		std::string valor = wcharToString(t->val); 
+		Expect(11 /* "_" */);
+		Expect(_ident);
+		std::string palo = wcharToString(t->val); 
+		playerCards.push_back(valor + "_" + palo); 
+}
+
+void Parser::CartaDealer() {
+		ValorCarta();
+		std::string valor = wcharToString(t->val); 
+		Expect(11 /* "_" */);
+		Expect(_ident);
+		std::string palo = wcharToString(t->val); 
+		dealerCards.push_back(valor + "_" + palo); 
+}
+
+void Parser::ValorCarta() {
+		if (la->kind == _number) {
+			Get();
+		} else if (la->kind == _ident) {
+			Get();
+		} else SynErr(18);
+}
+
+void Parser::ListaCartasTrucoJugador() {
+		CartaTrucoJugador();
+		while (la->kind == 10 /* "," */) {
+			Get();
+			CartaTrucoJugador();
+		}
+}
+
+void Parser::ListaCartasTrucoOponente() {
+		CartaTrucoOponente();
+		while (la->kind == 10 /* "," */) {
+			Get();
+			CartaTrucoOponente();
+		}
+}
+
+void Parser::ListaCartasMesaJugador() {
+		if (la->kind == _ident || la->kind == _number) {
+			CartaMesaJugador();
+			while (la->kind == 10 /* "," */) {
+				Get();
+				CartaMesaJugador();
+			}
+		}
+}
+
+void Parser::ListaCartasMesaOponente() {
+		if (la->kind == _ident || la->kind == _number) {
+			CartaMesaOponente();
+			while (la->kind == 10 /* "," */) {
+				Get();
+				CartaMesaOponente();
+			}
+		}
+}
+
+void Parser::CartaTrucoJugador() {
+		ValorCarta();
+		std::string valor = wcharToString(t->val); 
+		Expect(11 /* "_" */);
+		Expect(_ident);
+		std::string palo = wcharToString(t->val); 
+		playerCards.push_back(valor + "_" + palo); 
+}
+
+void Parser::CartaTrucoOponente() {
+		ValorCarta();
+		std::string valor = wcharToString(t->val); 
+		Expect(11 /* "_" */);
+		Expect(_ident);
+		std::string palo = wcharToString(t->val); 
+		opponentCards.push_back(valor + "_" + palo); 
+}
+
+void Parser::CartaMesaJugador() {
+		ValorCarta();
+		std::string valor = wcharToString(t->val); 
+		Expect(11 /* "_" */);
+		Expect(_ident);
+		std::string palo = wcharToString(t->val); 
+		mesaJugadorCards.push_back(valor + "_" + palo); 
+}
+
+void Parser::CartaMesaOponente() {
+		ValorCarta();
+		std::string valor = wcharToString(t->val); 
+		Expect(11 /* "_" */);
+		Expect(_ident);
+		std::string palo = wcharToString(t->val); 
+		mesaOponenteCards.push_back(valor + "_" + palo); 
+}
+
+
+
+
+// If the user declared a method Init and a mehtod Destroy they should
+// be called in the contructur and the destructor respctively.
+//
+// The following templates are used to recognize if the user declared
+// the methods Init and Destroy.
+
+template<typename T>
+struct ParserInitExistsRecognizer {
+	template<typename U, void (U::*)() = &U::Init>
+	struct ExistsIfInitIsDefinedMarker{};
+
+	struct InitIsMissingType {
+		char dummy1;
+	};
+	
+	struct InitExistsType {
+		char dummy1; char dummy2;
+	};
+
+	// exists always
+	template<typename U>
+	static InitIsMissingType is_here(...);
+
+	// exist only if ExistsIfInitIsDefinedMarker is defined
+	template<typename U>
+	static InitExistsType is_here(ExistsIfInitIsDefinedMarker<U>*);
+
+	enum { InitExists = (sizeof(is_here<T>(NULL)) == sizeof(InitExistsType)) };
+};
+
+template<typename T>
+struct ParserDestroyExistsRecognizer {
+	template<typename U, void (U::*)() = &U::Destroy>
+	struct ExistsIfDestroyIsDefinedMarker{};
+
+	struct DestroyIsMissingType {
+		char dummy1;
+	};
+	
+	struct DestroyExistsType {
+		char dummy1; char dummy2;
+	};
+
+	// exists always
+	template<typename U>
+	static DestroyIsMissingType is_here(...);
+
+	// exist only if ExistsIfDestroyIsDefinedMarker is defined
+	template<typename U>
+	static DestroyExistsType is_here(ExistsIfDestroyIsDefinedMarker<U>*);
+
+	enum { DestroyExists = (sizeof(is_here<T>(NULL)) == sizeof(DestroyExistsType)) };
+};
+
+// The folloing templates are used to call the Init and Destroy methods if they exist.
+
+// Generic case of the ParserInitCaller, gets used if the Init method is missing
+template<typename T, bool = ParserInitExistsRecognizer<T>::InitExists>
+struct ParserInitCaller {
+	static void CallInit(T *t) {
+		// nothing to do
+	}
+};
+
+// True case of the ParserInitCaller, gets used if the Init method exists
+template<typename T>
+struct ParserInitCaller<T, true> {
+	static void CallInit(T *t) {
+		t->Init();
+	}
+};
+
+// Generic case of the ParserDestroyCaller, gets used if the Destroy method is missing
+template<typename T, bool = ParserDestroyExistsRecognizer<T>::DestroyExists>
+struct ParserDestroyCaller {
+	static void CallDestroy(T *t) {
+		// nothing to do
+	}
+};
+
+// True case of the ParserDestroyCaller, gets used if the Destroy method exists
+template<typename T>
+struct ParserDestroyCaller<T, true> {
+	static void CallDestroy(T *t) {
+		t->Destroy();
+	}
+};
+
+void Parser::Parse() {
+	t = NULL;
+	la = dummyToken = new Token();
+	la->val = coco_string_create(L"Dummy Token");
+	Get();
+	Cardrep();
+	Expect(0);
+}
+
+Parser::Parser(Scanner *scanner) {
+	maxT = 16;
+
+	ParserInitCaller<Parser>::CallInit(this);
+	dummyToken = NULL;
+	t = la = NULL;
+	minErrDist = 2;
+	errDist = minErrDist;
+	this->scanner = scanner;
+	errors = new Errors();
+}
+
+bool Parser::StartOf(int s) {
+	const bool T = true;
+	const bool x = false;
+
+	static bool set[1][18] = {
+		{T,x,x,x, x,x,x,x, x,x,x,x, x,x,x,x, x,x}
+	};
+
+
+
+	return set[s][la->kind];
+}
+
+Parser::~Parser() {
+	ParserDestroyCaller<Parser>::CallDestroy(this);
+	delete errors;
+	delete dummyToken;
+}
+
+Errors::Errors() {
+	count = 0;
+}
+
+void Errors::SynErr(int line, int col, int n) {
+	wchar_t* s;
+	switch (n) {
+			case 0: s = coco_string_create(L"EOF expected"); break;
+			case 1: s = coco_string_create(L"ident expected"); break;
+			case 2: s = coco_string_create(L"number expected"); break;
+			case 3: s = coco_string_create(L"\"JUEGO\" expected"); break;
+			case 4: s = coco_string_create(L"\"PREDECIR\" expected"); break;
+			case 5: s = coco_string_create(L"\"jugada\" expected"); break;
+			case 6: s = coco_string_create(L"\"BLACKJACK_JUGADOR\" expected"); break;
+			case 7: s = coco_string_create(L"\"[\" expected"); break;
+			case 8: s = coco_string_create(L"\"]\" expected"); break;
+			case 9: s = coco_string_create(L"\"BLACKJACK_DEALER\" expected"); break;
+			case 10: s = coco_string_create(L"\",\" expected"); break;
+			case 11: s = coco_string_create(L"\"_\" expected"); break;
+			case 12: s = coco_string_create(L"\"TRUCO_JUGADOR\" expected"); break;
+			case 13: s = coco_string_create(L"\"TRUCO_OPONENTE\" expected"); break;
+			case 14: s = coco_string_create(L"\"TRUCO_MESA_JUGADOR\" expected"); break;
+			case 15: s = coco_string_create(L"\"TRUCO_MESA_OPONENTE\" expected"); break;
+			case 16: s = coco_string_create(L"??? expected"); break;
+			case 17: s = coco_string_create(L"invalid Estado"); break;
+			case 18: s = coco_string_create(L"invalid ValorCarta"); break;
+
+		default:
+		{
+			wchar_t format[20];
+			coco_swprintf(format, 20, L"error %d", n);
+			s = coco_string_create(format);
+		}
+		break;
+	}
+	wprintf(L"-- line %d col %d: %ls\n", line, col, s);
+	coco_string_delete(s);
+	count++;
+}
+
+void Errors::Error(int line, int col, const wchar_t *s) {
+	wprintf(L"-- line %d col %d: %ls\n", line, col, s);
+	count++;
+}
+
+void Errors::Warning(int line, int col, const wchar_t *s) {
+	wprintf(L"-- line %d col %d: %ls\n", line, col, s);
+}
+
+void Errors::Warning(const wchar_t *s) {
+	wprintf(L"%ls\n", s);
+}
+
+void Errors::Exception(const wchar_t* s) {
+	wprintf(L"%ls", s); 
+	exit(1);
+}
+
+
---
+++ CocoR v4/Parser.h
@@ -0,0 +1,96 @@
+
+
+#if !defined(COCO_PARSER_H__)
+#define COCO_PARSER_H__
+
+
+
+#include "Scanner.h"
+#include <vector>
+#include <string>
+
+std::string wcharToString(const wchar_t* wstr);
+
+// Variables globales para almacenar el estado del juego
+extern std::string gameType;
+extern std::vector<std::string> playerCards;
+extern std::vector<std::string> dealerCards;
+
+// Para Truco
+extern std::vector<std::string> opponentCards;
+extern std::vector<std::string> mesaJugadorCards;
+extern std::vector<std::string> mesaOponenteCards;
+
+
+class Errors {
+public:
+	int count;			// number of errors detected
+
+	Errors();
+	void SynErr(int line, int col, int n);
+	void Error(int line, int col, const wchar_t *s);
+	void Warning(int line, int col, const wchar_t *s);
+	void Warning(const wchar_t *s);
+	void Exception(const wchar_t *s);
+
+}; // Errors
+
+class Parser {
+private:
+	enum {
+		_EOF=0,
+		_ident=1,
+		_number=2
+	};
+	int maxT;
+
+	Token *dummyToken;
+	int errDist;
+	int minErrDist;
+
+	void SynErr(int n);
+	void Get();
+	void Expect(int n);
+	bool StartOf(int s);
+	void ExpectWeak(int n, int follow);
+	bool WeakSeparator(int n, int syFol, int repFol);
+
+public:
+	Scanner *scanner;
+	Errors  *errors;
+
+	Token *t;			// last recognized token
+	Token *la;			// lookahead token
+
+
+
+	Parser(Scanner *scanner);
+	~Parser();
+	void SemErr(const wchar_t* msg);
+
+	void Cardrep();
+	void Estado();
+	void EstadoBlackjack();
+	void EstadoTruco();
+	void ListaCartasJugador();
+	void ListaCartasDealer();
+	void CartaJugador();
+	void CartaDealer();
+	void ValorCarta();
+	void ListaCartasTrucoJugador();
+	void ListaCartasTrucoOponente();
+	void ListaCartasMesaJugador();
+	void ListaCartasMesaOponente();
+	void CartaTrucoJugador();
+	void CartaTrucoOponente();
+	void CartaMesaJugador();
+	void CartaMesaOponente();
+
+	void Parse();
+
+}; // end Parser
+
+
+
+#endif
+
